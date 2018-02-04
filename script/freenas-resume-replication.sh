8b1248107f430c6873b25d9ba25adf1ef0281d4c
Youngmo Eugene Kwon <youngmo.kwon777@gmail.com>
2018-02-04 17:22:42 +0900

    Create freenas-resume-replication.sh

---
+++ script/freenas-resume-replication.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SSH=/usr/local/bin/ssh
+REMOTE_IP=192.168.0.203
+REMOTE_PORT=22
+DATASET_OR_ZVOL=nfs-vmstore-01
+SSH_CMD_OPTIONS="-ononeenabled=yes -ononeswitch=yes -i /data/ssh/replication -o BatchMode=yes -o StrictHostKeyChecking=yes -o ConnectTimeout=7"
+RECEIVE_RESUME_TOKEN=""
+ZFS_EXIT_CODE=1
+
+echo $RESUME_TOKEN
+
+while [ $ZFS_EXIT_CODE -gt 0 ]
+do
+    RECEIVE_RESUME_TOKEN=`$SSH $SSH_CMD_OPTIONS -p $REMOTE_PORT $REMOTE_IP "zfs get -p receive_resume_token | grep $DATASET_OR_ZVOL" 2> /dev/null | awk '{print $3'}`
+    echo '['`date`']Recieved resume token: '$RECEIVE_RESUME_TOKEN
+    echo '['`date`']Resuming replication...'
+    /sbin/zfs send -t $RECEIVE_RESUME_TOKEN | /usr/local/bin/pigz | /usr/local/bin/pipewatcher $$ | /usr/local/bin/ssh $SSH_CMD_OPTIONS -p $REMOTE_PORT $REMOTE_IP "/usr/bin/env pigz -d | /sbin/zfs receive -s -F -d 'eugene-pool/replication-remote' && echo Succeeded"
+    ZFS_EXIT_CODE=$?
+    echo '['`date`']Communication broken, ZFS_EXIT_CODE ='$ZFS_EXIT_CODE
+    sleep 5
+done
