37a2ae2d4930835372b9d1f96c2787a7245d4557
Youngmo Eugene Kwon <youngmo.kwon777@gmail.com>
2018-02-06 04:01:52 +0900

    Update freenas-resume-replication.sh

--- script/freenas-resume-replication.sh
+++ script/freenas-resume-replication.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
 SSH=/usr/local/bin/ssh
-REMOTE_IP=192.168.0.203
-REMOTE_PORT=22
 DATASET_OR_ZVOL=$1
+REMOTE_IP=$2
+REMOTE_PORT=$3
+TARGET_LOCATION=$4
 SSH_CMD_OPTIONS="-ononeenabled=yes -ononeswitch=yes -i /data/ssh/replication -o BatchMode=yes -o StrictHostKeyChecking=yes -o ConnectTimeout=7"
 RECEIVE_RESUME_TOKEN=""
 ZFS_EXIT_CODE=1
 
 echo 'DATASET_OR_ZVOL='$1
+echo 'TARGET_IP='$2
+echo 'TARGET_PORT='$3
+echo 'TARGET_LOCATION='$4
 
 /sbin/zfs send -V -p -i $DATASET_OR_ZVOL | /usr/local/bin/pigz | /usr/local/bin/pipewatcher $$ | /usr/local/bin/ssh $SSH_CMD_OPTIONS -p $REMOTE_PORT $REMOTE_IP "/usr/bin/env pigz -d | /sbin/zfs receive -F -d 'backup-pool/replication-local' && echo Succeeded"
 $ZFS_EXIT_CODE=$?
